2f749dc347b2fde37bba1d2cacf3d53b426e7796
zhaowei.zhang01 <zhaowei.zhang01@hand-china.com>
2019-03-05 16:08:33 +0800

    回退测试

--- src/main/java/com/zzw/usermanage/controller/UserController.java
+++ src/main/java/com/zzw/usermanage/controller/UserController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 public class UserController {
+    //aaaaaaaaaaaaaaaaaaaaaaa
+
+
 
     @Autowired
     private UserServicel userService;
