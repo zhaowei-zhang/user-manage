10e2c9ba9f2258cb8ca380473f639803ab78dcb9
zhaowei.zhang01 <zhaowei.zhang01@hand-china.com>
2019-03-06 10:04:59 +0800

    api文档测试成功-文档

---
+++ apidoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "用户管理",
+  "version": "1.0.0",
+  "description": "用户管理系统API文档",
+  "title": "用户管理API文档",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-06T02:01:47.828Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
